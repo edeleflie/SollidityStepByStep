61cd7b85a82d834f6ddbca611e5dc87e0ac828e6
Etienne Deleflie <Meiling@Meilings-MacBook-Pro.local>
2017-11-09 11:31:25 +1100

    bad use of equality operator

--- 05_accept_fixed_amount_better.sol
+++ 05_accept_fixed_amount_better.sol
@@ -13,7 +13,7 @@ contract WishingWell {
     function deposit() payable public returns (uint256){
 
         // the fixed amount we would like to ensure: 1 Finney
-        uint _amount == 1000000000000000;
+        uint _amount = 1000000000000000;
 
         // Check that the amount sent is the same as the fixed amount
         require( msg.value == _amount);
