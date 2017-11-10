5c99bddd9b76d9cdb3ce072b394b0ee19aff35c8
Etienne Deleflie <Meiling@Meilings-MacBook-Pro.local>
2017-11-10 12:23:28 +1100

    Removed wrong comment

--- 01_receive_currency.sol
+++ 01_receive_currency.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.0;
 /* 
  * To send Ether to a contract only requires making a function 'payable'
  *
- * It is the sender who decides who much Ether is sent, how much gas to dedicate.
- * If using a Javascript client, this is what the call might look like:
+ * It is the sender who decides who much Ether is sent.
+ * If using a Javascript client, this is what the call to deposit might look like:
  * 
  * wishingWellContract.deposit({from: eth.accounts[0], value: web3.toWei(1.0, "ether")});
  *
