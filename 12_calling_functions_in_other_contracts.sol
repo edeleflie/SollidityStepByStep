ac9951a6df2c878649c1a4a521c93d6eed14b1ce
Etienne Deleflie <Meiling@Meilings-MacBook-Pro.local>
2017-11-17 09:12:28 +1100

    Removed dependency on finding out contract type

--- 12_calling_functions_in_other_contracts.sol
+++ 12_calling_functions_in_other_contracts.sol
@@ -12,25 +12,31 @@ pragma solidity ^0.4.0;
 
 contract ArtBank {
 
-	ArtWork[] artworks;
+	mapping (address => ArtBank) artworks;
+	uint artworks_count = 0;
 
 	function registerNewArtwork(bytes32 artist, bytes32 workName, uint yearCreated) public {
-		ArtWork newWork = new ArtWork(artist, workName, yearCreated);
-		artworks.push(newWork);
+		ArtWork newWork = new ArtWork(msg.sender, artist, workName, yearCreated);
+		artworks_count ++;
+		artworks[address(newWork)] = newWork;
 	}
 
 	// This function retrieves a contract from our list of artworks, and effectuates
 	// a change in ownership
-	function changeOwnership(address artWorkAddress, address newOwner) returns (bytes32){
-		// First, we re-create a local handle to the concerned contract
-		// this is done simply by passing in the contract address as a constructor!
-		ArtWork work = ArtWork(artWorkAddress);
-		if (work)
-        return work.getWorkName();
+	function changeOwnership(address artWorkAddress, address newOwner) returns (bool){
+		// First, we retrieve the contract at that address
+		ArtWork work = artworks[artWorkAddress];
+		// test that it is not dud
+		if (work > 0) {
+			work.setNewOwner(newOwner);
+			return true;
+		} else {
+	        return false;
+	    }
 	}
 
 	function countArtWorks() public view returns (uint) {
-        return artworks.length;
+        return artworks_count;
 	}
 
 }
@@ -39,13 +45,19 @@ contract ArtWork {
 	bytes32 public _artist;
 	bytes32 public _workName;
 	uint public _yearCreated;
+	address public _currentOwner;
 	
-	function ArtWork(bytes32 artist, bytes32 workName, uint yearCreated) public{
+	function ArtWork(address owner, bytes32 artist, bytes32 workName, uint yearCreated) public{
 		_artist = artist;
 		_workName = workName;
 		_yearCreated = yearCreated;
+		_currentOwner = owner;
 	}
 	
+	function setNewOwner(address newOwner) public {
+	    _currentOwner = newOwner;
+	}
+
 	function getWorkName() public returns (bytes32) {
 	    return _workName;
 	}
